452f745b71e06ff40f985ba5b053dc74a8b250f1
romain(samsung) <romain.ligneul@gmail.com>
2025-07-02 15:35:47 +0200

    db

--- server/app.js
+++ server/app.js
@@ -2,14 +2,33 @@
 var fs = require("fs")
 var path = require("path")
 var express = require('express')
-var cors = require('cors')
 require('dotenv').config()
+const bodyParser = require("body-parser");
+
+var mongoose = require("mongoose");
+
+const yourName="Romain"
+const dbSchema = new mongoose.Schema({}, {
+  strict: false,
+  collection: yourName // bind schema to specific collection
+});
+const dbModel = mongoose.model(yourName, dbSchema);
+
+mongoose.connect(process.env.MONGODB_URI);
+
+var db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function callback() {
+  console.log("database opened");
+});
 
 // --- INSTANTIATE THE APP
 var app = express();
 
+app.use(bodyParser.json());
+
 // manage cors policy
-app.use(cors())
 
 app.use(express.static(__dirname + '/public/'));
 
@@ -24,6 +43,16 @@ app.get('/expNOW', function (request, response) {
 app.get('/dummyRoute', function (request, response) {
   response.render('dummy.html');
 });
+
+app.get('/', function (request, response) {
+  response.render('welcome.html');
+});
+
+app.post('/postData', (req, res) => {
+  dbModel.create(req.body)
+  res.json({status: "OK"})
+});
+
 
 // set view engigne
 app.engine('html', require('ejs').renderFile);
